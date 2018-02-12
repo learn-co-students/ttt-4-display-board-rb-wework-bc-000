2432f1cad5772bd7b783eb7a40a06bdee156d750
Darcy McCartney <darcy.mccartney@wework.com>
2018-02-12 04:34:26 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,15 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def display_board(space)
+  puts " #{space[0]} | #{space[1]} | #{space[2]} "
+  puts "-----------"
+  puts " #{space[3]} | #{space[4]} | #{space[5]} "
+  puts "-----------"
+  puts " #{space[6]} | #{space[7]} | #{space[8]} "
+end
+
+display_board(board)
+
