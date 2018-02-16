4597c4442c6f35b0b6863ff78aad79bcd99119d1
Emily Lazio <emily.lazio@wework.com>
2018-02-16 00:52:49 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+ def display_board(board)
+   puts row_one = " #{board[0]} | #{board[1]} | #{board[2]} "
+   puts middle = "-----------"
+   puts row_two = " #{board[3]} | #{board[4]} | #{board[5]} "
+   puts middle = "-----------"
+   puts row_three = " #{board[6]} | #{board[7]} | #{board[8]} "
+ end 
+   
+
+
